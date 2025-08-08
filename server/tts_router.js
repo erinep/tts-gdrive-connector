e1cb2e2c6d9bc9cd16138fdc545d7e71b9462672
Erin Patterson <epatterson7@myseneca.ca>
2025-08-08 18:40:08 -0400

    throw errors :)

--- server/tts_router.js
+++ server/tts_router.js
@@ -1,20 +1,29 @@
-const CURRENT_SERVICE = "eleven_labs"
+const CURRENT_SERVICE = "google"
 
 function fetchAudioBase64(text, voice) {
+  if (CURRENT_SERVICE === "google") {
+    throw new Error("google fetchAudioBase64 not implemented")
+  }
 
   if (CURRENT_SERVICE === "eleven_labs"){
-      return el_fetchAudioBase64(text, voice);
+    return el_fetchAudioBase64(text, voice);
   }
 }
 
 function test_tts_connection() {
-    if (CURRENT_SERVICE === "eleven_labs"){
-        return el_testElevenLabsUser();
-    }
+  if (CURRENT_SERVICE === "google") {
+    throw new Error("google test_tts_connection not implemented")
+  }
+  if (CURRENT_SERVICE === "eleven_labs"){
+    return el_testElevenLabsUser();
+  }
 }
 
 function getAIVoiceList() {
-    if (CURRENT_SERVICE === "eleven_labs"){
-        return el_getVoicesElevenLabs();
-    }
+  if (CURRENT_SERVICE === "google") {
+    throw new Error("google getAIVoiceList not implemented")
+  }
+  if (CURRENT_SERVICE === "eleven_labs"){
+    return el_getVoicesElevenLabs();
+  }
 }
