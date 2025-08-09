1fb4ccb288f5db28dd8ebdea4c23807b04b88931
Erin Patterson <epatterson7@myseneca.ca>
2025-08-09 15:54:34 -0400

    accent selection + added static ElevenLab voices

--- server/tts_ElevenLabs.js
+++ server/tts_ElevenLabs.js
@@ -1,3 +1,4 @@
+const EL_VOICES = {voices: [{"name":"William Shanks","id":"8Es4wFxsDlHBmFWAOWRS"},{"name":"Christopher","id":"G17SuINrv2H9FC6nvetn"},{"name":"Benjamin - Deep, Warm, Calming","id":"LruHrtVF6PSyGItzMNHS"},{"name":"John Morgan - Old West Southern Cowboy","id":"ruirxsoakN0GWmGNIo04"},{"name":"Hey Its Brad - Eyewitness Interview","id":"MYiFAKeVwcvm4z9VsFAR"},{"name":"Creature - Goblin Mythical Monster","id":"Z7RrOqZFTyLpIlzCgfsp"},{"name":"Johnny Dynamite - 80s Radio DJ","id":"CeNX9CMwmxDxUF5Q2Inm"},{"name":"Jessica Anne Bogart - Character and Animation","id":"flHkNRp1BlvT73UL6gyz"},{"name":"Northern Terry","id":"wo6udizrrtpIxWGp2qJk"},{"name":"Juniper","id":"aMSt68OGf4xUZAnLpTU8"},{"name":"Julian - deep rich mature British voice","id":"7p1Ofvcwsv7UBPoFNcpI"},{"name":"Wyatt- Wise Rustic Cowboy","id":"YXpFCvM1S3JbWEJhoskW"},{"name":"David Castlemore - Newsreader and Educator","id":"XjLkpWUlnhS8i7gGz3lZ"},{"name":"Michael C. Vincent","id":"uju3wxzG5OhpWcoi3SMy"},{"name":"John Doe - Deep","id":"EiNlNiXeDU1pqqOPrYMO"},{"name":"Adam Stone - late night radio","id":"NFG5qt843uXKj4pFvR7C"},{"name":"Danielle - Canadian Narrator","id":"FVQMzxJGPUBtfz1Azdoy"},{"name":"Ana-Rita","id":"wJqPPQ618aTW29mptyoc"},{"name":"Jane - Professional Audiobook Reader","id":"RILOU7YmBhvwJGDGjNmP"},{"name":"Monika Sogam - Suspense and Horror Storyteller","id":"6qL48o1LBmtR94hIYAQh"},{"name":"Hope - soothing narrator","id":"iCrDUkL56s3C8sCRl7wb"},{"name":"Ivy - Free Spirit","id":"i4CzbCVWoqvD0P1QJCUL"},{"name":"Amelia","id":"ZF6FPAbjXT4488VcRRnw"},{"name":"Grandfather Joe -  Gentle, warm & wise","id":"0lp4RIz96WD1RUtvEu3Q"},{"name":"Grandpa Spuds Oxley","id":"NOpBlnGInO9m6vDvFkFC"},{"name":"Brian - Very Realistic Reader","id":"UGTtbzgh3HObxRjWaSpr"}]};
 
 function el_callTextToSpeech(text, voice) {
   const apiKey = getApiKeyForUser();
@@ -39,14 +40,5 @@ function el_testElevenLabsUser() {
 }
 
 function el_getVoicesElevenLabs() {
-  const apiKey = getApiKeyForUser();
-  if (!apiKey) throw new Error("API key missing.");
-  const res = UrlFetchApp.fetch("https://api.elevenlabs.io/v2/voices", {
-    method: "get",
-    headers: {"xi-api-key": apiKey},
-  });
-  if (res.getResponseCode() !== 200) { throw new Error("HTTP ERROR: " + res.getContentText()); }
-  const content = res.getContentText();
-  // Logger.log(content)
-  return JSON.parse(content);
+  return EL_VOICES;
 }
--- server/tts_router.js
+++ server/tts_router.js
@@ -10,7 +10,7 @@ function setCurrentService(value) {
   else throw new Error("service id '" + value + "' not found.");
 }
 
-function fetchAudioBase64(text, voice) {
+function fetchAudioBase64(text, voice, locale) {
   const apiKey = getApiKeyForUser();
   if (!apiKey) throw new Error ("API key missing");  
   if (!text|| text === '(No text selected)') throw new Error ("No Text selected");
@@ -19,7 +19,7 @@ function fetchAudioBase64(text, voice) {
 
   let serve = getCurrentService();
   if (serve === "google-tts") {
-    return g_callTextToSpeech(text, voice, "en-US");
+    return g_callTextToSpeech(text, voice, locale);
   } else if (serve === "eleven_labs"){
     return el_callTextToSpeech(text, voice);
   } else {
