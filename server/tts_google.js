7e5c4f15980b84347febcf87c7ecade6f4ef5406
Erin Patterson <epatterson7@myseneca.ca>
2025-08-09 20:53:42 -0400

    added google studio voices

--- server/tts_google.js
+++ server/tts_google.js
@@ -1,4 +1,8 @@
-const google_voices = {voices: ["Achernar","Achird","Algenib","Algieba","Alnilam","Aoede","Autonoe","Callirrhoe","Charon","Despina","Enceladus","Erinome","Fenrir","Gacrux","Iapetus","Kore","Laomedeia","Leda","Orus","Puck","Pulcherrima","Rasalgethi","Sadachbia","Sadaltager","Schedar","Sulafat","Umbriel","Vindemiatrix","Zephyr","Zubenelgenubi"]};
+const google_voices = {voices: ["Studio-Male","Studio-Female","Achernar","Achird","Algenib","Algieba","Alnilam","Aoede","Autonoe","Callirrhoe","Charon","Despina","Enceladus","Erinome","Fenrir","Gacrux","Iapetus","Kore","Laomedeia","Leda","Orus","Puck","Pulcherrima","Rasalgethi","Sadachbia","Sadaltager","Schedar","Sulafat","Umbriel","Vindemiatrix","Zephyr","Zubenelgenubi"]};
+const studioVoiceLookup = {
+  "Studio-Female": {"en-US": "en-US-Studio-O", "en-GB": "en-GB-Studio-C"},
+  "Studio-Male": {"en-US": "en-US-Studio-Q", "en-GB": "en-GB-Studio-B"}
+}
 
 function g_getVoices() {
   return google_voices;
@@ -11,6 +15,15 @@ function g_testConnection() {
 function g_callTextToSpeech(text, voiceName, languageCode, speed) {
   const apiKey = getApiKeyForUser();
   const url = "https://texttospeech.googleapis.com/v1beta1/text:synthesize?key="+apiKey;
+
+  let voice_id = ""
+
+  if(voiceName.includes("Studio")){
+    if (languageCode=== "en-AU") throw new Error("Australian Studio Voice not supported");
+    voice_id = studioVoiceLookup[voiceName][languageCode];
+  } else {
+    voice_id = languageCode + "-Chirp3-HD-" + voiceName;
+  }
 
   const options = {
     method: "post",
@@ -25,7 +38,7 @@ function g_callTextToSpeech(text, voiceName, languageCode, speed) {
       },
       "voice": {
         "languageCode": languageCode,
-        "name": languageCode + "-Chirp3-HD-" + voiceName,
+        "name": voice_id
       }
     })
   };
